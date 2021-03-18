10c363f51e473fa2ac201616226950d0c33b5728
Clemens Stich <ClemensStich@web.de>
2021-03-18 16:20:10 +0100

    - Enhanced LinkExtern.tsx to deal with self-target and children

--- runningdinner-client/packages/webapp/src/common/theme/LinkExtern.tsx
+++ runningdinner-client/packages/webapp/src/common/theme/LinkExtern.tsx
@@ -1,16 +1,21 @@
 import {Link, Typography} from "@material-ui/core";
 import React from "react";
+import {isStringNotEmpty, Parent} from "@runningdinner/shared";
 
-export interface LinkExternProps {
+export interface LinkExternProps extends Parent {
   href: string;
-  title: React.ReactNode;
+  title?: string
+  self?: boolean;
 }
 
-export default function LinkExtern({href, title}: LinkExternProps) {
+export default function LinkExtern({href, title, self, children}: LinkExternProps) {
+
+  const target = self ? "_self" : "_blank";
 
   return (
-      <Link href={href} color="primary" target={"_blank"}>
-        <Typography variant={"body2"} component={"span"}>{title}</Typography>
+      <Link href={href} color="primary" target={target}>
+        { isStringNotEmpty(title) && <Typography variant={"body2"} component={"span"}>{title}</Typography> }
+        { children }
       </Link>
   );
 }
