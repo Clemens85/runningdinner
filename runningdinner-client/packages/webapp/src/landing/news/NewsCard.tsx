0185638e588ea5c57a07029dcdf711d5b13da5b5
Clemens Stich <clemensstich@googlemail.com>
2021-11-19 17:09:44 +0100

    Beautified news list with avatar

--- runningdinner-client/packages/webapp/src/landing/news/NewsCard.tsx
+++ runningdinner-client/packages/webapp/src/landing/news/NewsCard.tsx
@@ -2,14 +2,19 @@ import {LocalDate} from "@runningdinner/shared";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import React from "react";
-import {CardHeader, makeStyles} from "@material-ui/core";
+import {Avatar, CardHeader, makeStyles} from "@material-ui/core";
 import {NewsItem} from "./NewsItemsHook";
 import { useLandingStyles } from "../LandingStyles";
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
-const useNewsStyles = makeStyles(() => ({
+const useNewsStyles = makeStyles((theme) => ({
   header: {
     paddingBottom: 0
-  }
+  },
+  newsIcon: {
+    color: '#fff',
+    backgroundColor: theme.palette.primary.main,
+  },
 }));
 
 export function NewsCard({title, content, date}: NewsItem) {
@@ -21,6 +26,7 @@ export function NewsCard({title, content, date}: NewsItem) {
     <Card className={landingStyles.teaserCard}>
       <CardHeader title={title}
                   className={newsStyles.header}
+                  avatar={<Avatar className={newsStyles.newsIcon}><AssignmentIcon /></Avatar>}
                   subheader={<LocalDate date={date} />} />
       <CardContent>
         <div>{content}</div>
