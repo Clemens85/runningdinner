cf3ac8a1a53cf454caaea39b349ad0be0fdc94ac
Clemens Stich <clemens.stich@otto.de>
2022-04-07 13:28:50 +0200

    Use docker host IP for NGINX backend proxy passing

--- runningdinner-backend/infrastructure/local/start-dev-env.sh
+++ runningdinner-backend/infrastructure/local/start-dev-env.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
+
+# Gets only the IP of the docker host:
+dockerHostIp=($(docker run nginx getent hosts host.docker.internal))
+# (Over-)write .env docker compose file:
+echo "DOCKER_HOST_IP=$dockerHostIp" > .env
+
 docker-compose up -d
 sleep 2s
 docker exec -d db bash /docker-entrypoint-initdb.d/init-db.sh
