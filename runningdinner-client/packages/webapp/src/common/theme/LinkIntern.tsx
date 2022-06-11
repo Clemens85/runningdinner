f5500bf0cb90ad6dbe9783aacf7b7341e230154b
Clemens Stich <clemensstich@googlemail.com>
2022-06-11 13:48:49 +0200

    - Enhanced LinkInter by href property

--- runningdinner-client/packages/webapp/src/common/theme/LinkIntern.tsx
+++ runningdinner-client/packages/webapp/src/common/theme/LinkIntern.tsx
@@ -6,15 +6,16 @@ import {isStringEmpty, Parent} from "@runningdinner/shared";
 export interface LinkInternProps extends Parent {
   pathname: string;
   color?: string;
+  href?: string;
 }
 
-export default function LinkIntern({pathname, color, children}: LinkInternProps) {
+export default function LinkIntern({pathname, color, href, children}: LinkInternProps) {
 
   const colorToSet = isStringEmpty(color) ? "primary" : color;
 
   return (
       // @ts-ignore
-      <Link to={{ pathname: pathname}} component={RouterLink} color={colorToSet}>
+      <Link to={{ pathname: pathname}} component={RouterLink} color={colorToSet} href={href}>
         {children}
       </Link>
   );
