3beaf7fb666e84b8ee0e0a69f38bef64328055bb
Clemens Stich <clemensstich@googlemail.com>
2022-10-17 23:44:12 +0200

    - Fixed bug for showing registration finished page

--- runningdinner-client/packages/webapp/src/landing/LandingNavigationHook.ts
+++ runningdinner-client/packages/webapp/src/landing/LandingNavigationHook.ts
@@ -2,7 +2,7 @@ import { RUNNING_DINNER_EVENTS_PATH } from "../common/mainnavigation/NavigationP
 import {useNavigate} from "react-router-dom";
 
 export function generateRegistrationFinishedPath(publicDinnerId: string) {
-  return `${RUNNING_DINNER_EVENTS_PATH}/${publicDinnerId}/registration-finished`;
+  return `/${RUNNING_DINNER_EVENTS_PATH}/${publicDinnerId}/registration-finished`;
 }
 
 export function useLandingNavigation() {
