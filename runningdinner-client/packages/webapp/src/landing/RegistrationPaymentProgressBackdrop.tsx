89fc6a229346a0a18314b8ab8e7f01ad29a2e370
Clemens Stich <clemensstich@googlemail.com>
2023-08-09 12:59:57 +0200

    - Removed usage of useStyles API in RegistrationPaymentProgressBackdrop.tsx

--- runningdinner-client/packages/webapp/src/landing/RegistrationPaymentProgressBackdrop.tsx
+++ runningdinner-client/packages/webapp/src/landing/RegistrationPaymentProgressBackdrop.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import { Theme } from '@mui/material/styles';
-import makeStyles from '@mui/styles/makeStyles';
-import createStyles from '@mui/styles/createStyles';
-import {Typography} from "@mui/material";
 import {useTranslation} from "react-i18next";
 import Paragraph from "../common/theme/typography/Paragraph";
 
-const useBackdropStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      color: '#fff',
-    },
-  }),
-);
-
 export function RegistrationPaymentProgressBackdrop() {
 
-  const classes = useBackdropStyles();
   const {t} = useTranslation("landing");
 
   return (
-    <Backdrop className={classes.backdrop} open={true}>
+    <Backdrop open={true}
+              sx={{ zIndex: (theme) => theme.zIndex.drawer + 1,
+                    color: '#ffffff' }}>
       <CircularProgress color="inherit" />
       <Paragraph>&nbsp; {t("landing:payment_processing")}</Paragraph>
     </Backdrop>
