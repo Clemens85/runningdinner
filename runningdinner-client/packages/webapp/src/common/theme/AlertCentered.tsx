bb169d8b212f44befe7d801d50983fc7f352d752
Clemens Stich <clemensstich@googlemail.com>
2023-09-12 20:18:22 +0200

    - Migrated AlertCentered to styled component

--- runningdinner-client/packages/webapp/src/common/theme/AlertCentered.tsx
+++ runningdinner-client/packages/webapp/src/common/theme/AlertCentered.tsx
@@ -1,18 +1,11 @@
-import makeStyles from '@mui/styles/makeStyles';
 import { Alert } from '@mui/material';
-import { AlertProps } from '@mui/lab';
 import React from "react";
+import {styled} from "@mui/material/styles";
 
-const useAlertStyles = makeStyles(() => ({
-  message: {
+const AlertCentered = styled(Alert)( {
+  '& .MuiAlert-message': {
     textAlign: 'center',
     width: "100%"
   }
-}));
-
-export default function AlertCentered(props: AlertProps) {
-  const styles = useAlertStyles();
-  return (
-    <Alert {...props} classes={{ message: styles.message }} />
-  );
-}
+});
+export default AlertCentered;
