6039094dff75de7c7a5e797d390495ca23dc9b3e
Clemens Stich <clemensstich@googlemail.com>
2024-02-28 22:28:05 +0100

    - Added easy jump links into new messaging service in ParticipantsPage.tsx for local dev

--- runningdinner-client/webapp/src/common/EnvService.ts
+++ runningdinner-client/webapp/src/common/EnvService.ts
@@ -1,4 +1,4 @@
 export function isLocalDevEnv() {
   const currentUrl = window.location.href;
-  return currentUrl.indexOf("localhost:3000") >= 0;
+  return currentUrl.indexOf("localhost:3000") >= 0 || currentUrl.indexOf("http://localhost") >= 0;
 }
