6e94c503a9f7cbfa7f1ef1f8c3986e81b5d52003
Clemens Stich <clemensstich@googlemail.com>
2024-09-11 22:07:53 +0200

    - Fix for not working validation in wizard rest controller when entering public settings

--- runningdinner-backend/src/main/java/org/runningdinner/wizard/rest/CreateRunningDinnerWizardServiceRest.java
+++ runningdinner-backend/src/main/java/org/runningdinner/wizard/rest/CreateRunningDinnerWizardServiceRest.java
@@ -1,8 +1,14 @@
 package org.runningdinner.wizard.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.runningdinner.admin.AfterPartyLocationService;
 import org.runningdinner.admin.RunningDinnerService;
 import org.runningdinner.admin.rest.RunningDinnerAdminTO;
+import org.runningdinner.common.Issue;
+import org.runningdinner.common.IssueList;
+import org.runningdinner.common.IssueType;
+import org.runningdinner.common.exception.ValidationException;
 import org.runningdinner.common.service.UrlGenerator;
 import org.runningdinner.contract.Contract;
 import org.runningdinner.core.AfterPartyLocation;
@@ -17,9 +23,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -42,8 +45,9 @@ public class CreateRunningDinnerWizardServiceRest {
 	@PutMapping(value = "/validate/options", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void validateOptions(
 			@Valid @RequestBody OptionsTO options,
-			@RequestParam @Valid @NotNull(message = "error.required.date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate runningDinnerDate) {
+			@RequestParam /*@Valid @NotNull(message = "error.required.date")*/ @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate runningDinnerDate) {
 
+		checkRunningDinnerDateParamNotNull(runningDinnerDate);
 		List<MealClass> meals = options.getMeals();
 		createWizardService.validateMeals(meals, runningDinnerDate);
 	}
@@ -51,9 +55,21 @@ public class CreateRunningDinnerWizardServiceRest {
 	@PutMapping(value = "/validate/publicsettings", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void validatePublicSettings(
 			@Valid @RequestBody PublicSettingsTO publicSettings,
-			@RequestParam @Valid @NotNull(message = "error.required.date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate runningDinnerDate) {
-		
+			@RequestParam /*@Valid @NotNull(message = "error.required.date")*/ @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate runningDinnerDate) {
+
+		checkRunningDinnerDateParamNotNull(runningDinnerDate);
 	  RunningDinnerService.validatePublicSettings(publicSettings, runningDinnerDate);
+	}
+
+	/**
+	 * @Valid @NotNull annotation in rest method used with also @valid @RequestBody crashes complete validation, see here: https://github.com/spring-projects/spring-framework/issues/32396.
+	 * We would have to implement an exception handler for HandlerMethodValidationException which is  much effort, hence we stick to this here.
+	 * @param runningDinnerDate
+	 */
+	private void checkRunningDinnerDateParamNotNull(LocalDate runningDinnerDate) {
+		if (runningDinnerDate == null) {
+			throw new ValidationException(new IssueList(new Issue("error.required.date", IssueType.VALIDATION)));
+		}
 	}
 
     @PutMapping(value = "/validate/afterpartylocation", consumes = MediaType.APPLICATION_JSON_VALUE)
