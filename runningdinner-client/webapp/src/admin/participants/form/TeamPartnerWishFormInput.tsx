daabbac02e29dba635deb5b764a624a39fec782d
Clemens Stich <clemensstich@googlemail.com>
2025-03-23 16:43:42 +0100

    - Fixed minor code findings

--- runningdinner-client/webapp/src/admin/participants/form/TeamPartnerWishFormInput.tsx
+++ runningdinner-client/webapp/src/admin/participants/form/TeamPartnerWishFormInput.tsx
@@ -1,7 +1,6 @@
 import {useTranslation} from "react-i18next";
 import {Grid} from "@mui/material";
 import FormTextField from "../../../common/input/FormTextField";
-import React from "react";
 
 export type TeamPartnerWishFormInputProps = {
   teamPartnerWishHelperText: string;
