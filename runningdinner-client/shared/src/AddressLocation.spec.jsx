af9ca18d09497f670a0610e15e6eacf53916ffb5
Clemens Stich <clemensstich@googlemail.com>
2025-03-29 13:51:09 +0100

    - Updated to latest vitest and vite and testing libraries - Re-activated tests and remove obsolete ones - Integrated tests into CI

--- runningdinner-client/shared/src/AddressLocation.spec.jsx
+++ runningdinner-client/shared/src/AddressLocation.spec.jsx
@@ -1,11 +1,9 @@
 // these imports are something you'd normally configure Jest to import for you
 // automatically. Learn more in the setup docs: https://testing-library.com/docs/react-testing-library/setup#cleanup
 import '@testing-library/jest-dom'
-// NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
 
 import React from 'react'
 import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'
 import {AddressLocation} from "./AddressLocation";
 
 test('City and Zip is rendered', () => {
---
+++ runningdinner-client/webapp/vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest/config" />
+import {defineConfig} from "vite";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.js'],
+  },
+})
