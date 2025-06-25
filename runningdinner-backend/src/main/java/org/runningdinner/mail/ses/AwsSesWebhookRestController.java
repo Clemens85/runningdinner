fa8f33f3c252435ee1d1b9c02f1bb340e3a009d9
Clemens Stich <clemensstich@googlemail.com>
2025-06-25 21:24:23 +0200

    - Fixed path for webhook

--- runningdinner-backend/src/main/java/org/runningdinner/mail/ses/AwsSesWebhookRestController.java
+++ runningdinner-backend/src/main/java/org/runningdinner/mail/ses/AwsSesWebhookRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ses/notifications")
+@RequestMapping("/rest/ses/notifications")
 public class AwsSesWebhookRestController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AwsSesWebhookRestController.class);
