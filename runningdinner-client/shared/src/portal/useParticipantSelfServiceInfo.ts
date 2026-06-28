539540c3595aaa4b9da7ba75d22d9a0728f876cd
Clemens Stich <clemensstich@googlemail.com>
2026-06-28 14:44:37 +0200

    - refetch data always on window focus

--- runningdinner-client/shared/src/portal/useParticipantSelfServiceInfo.ts
+++ runningdinner-client/shared/src/portal/useParticipantSelfServiceInfo.ts
@@ -8,5 +8,7 @@ export function useParticipantSelfServiceInfo(selfAdminId: string, participantId
     queryFn: () => fetchParticipantSelfServiceInfo(selfAdminId, participantId, portalToken!),
     enabled: !!(selfAdminId && participantId && portalToken),
     staleTime: 30_000,
+    // Always re-fetch when the user returns to this tab (e.g. after changing the team host in the new-tab flow)
+    refetchOnWindowFocus: 'always',
   });
 }
